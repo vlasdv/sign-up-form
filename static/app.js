9c48053ef94fde6667876039d6169adcb9fe02b4
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-12-10 15:53:15 +0100

    Adjust button color, clean app.js

--- static/app.js
+++ static/app.js
@@ -1,13 +1,10 @@
 const inputFields = document.querySelectorAll('input');
 
-console.log(inputFields);
-
 for (let input of inputFields) {
   input.addEventListener('focus', (event) => {
     const parentDiv = event.target.parentElement;
     parentDiv.style.border = '1px solid #111';    
     parentDiv.style.backgroundColor = '#f9f9f9';
-    
   });
 }
 
@@ -15,6 +12,6 @@ for (let input of inputFields) {
   input.addEventListener('blur', (event) => {
     const parentDiv = event.target.parentElement;
     parentDiv.style.border = '1px solid #aaa';
-    parentDiv.style.backgroundColor = '';
+    parentDiv.style.backgroundColor = '';  
   });
 }
