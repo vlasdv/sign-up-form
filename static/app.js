53c4573a37cc759b102ce9f220aa1460b55061dd
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-12-11 15:37:19 +0100

    Change design and structure of input forms

--- static/app.js
+++ static/app.js
@@ -1,17 +1,17 @@
 const inputFields = document.querySelectorAll('input');
 
-for (let input of inputFields) {
-  input.addEventListener('focus', (event) => {
-    const parentDiv = event.target.parentElement;
-    parentDiv.style.border = '1px solid #111';    
-    parentDiv.style.backgroundColor = '#f9f9f9';
-  });
-}
+// for (let input of inputFields) {
+//   input.addEventListener('focus', (event) => {
+//     const parentDiv = event.target.parentElement;
+//     parentDiv.style.border = '1px solid #111';    
+//     parentDiv.style.backgroundColor = '#f9f9f9';
+//   });
+// }
 
-for (let input of inputFields) {
-  input.addEventListener('blur', (event) => {
-    const parentDiv = event.target.parentElement;
-    parentDiv.style.border = '1px solid #aaa';
-    parentDiv.style.backgroundColor = '';  
-  });
-}
+// for (let input of inputFields) {
+//   input.addEventListener('blur', (event) => {
+//     const parentDiv = event.target.parentElement;
+//     parentDiv.style.border = '1px solid #aaa';
+//     parentDiv.style.backgroundColor = '';  
+//   });
+// }
